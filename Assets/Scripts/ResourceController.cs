8515137ebf01b5c1d26e0513d451a53baebb1d94
Duy Huynh Quoc <duyhq.zee@gmail.com>
2023-04-10 14:46:54 +0700

    [New] Increase golds by time

--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -6,14 +6,21 @@ using TMPro;
 public class ResourceController : MonoBehaviour
 {
     [SerializeField] int golds;
+    [SerializeField] int increasingGoldsByTime = 5;
     [SerializeField] TMP_Text goldTMP;
 
     void Start() {
         UpdateGoldText();
+        StartCoroutine(IncreaseGoldsByTime());
     }
 
     void Update() {
-        
+    }
+
+    IEnumerator IncreaseGoldsByTime() {
+        SetGolds(golds + increasingGoldsByTime);
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(IncreaseGoldsByTime());
     }
 
     public int GetGolds() {
